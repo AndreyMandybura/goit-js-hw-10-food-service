bc2f859c64f25d9ca79e530d5d90576aba3ac939
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2021-09-10 10:28:41 +0300

    Старт ДЗ

--- homework-07/src/js/task-01.js
+++
@@ -1,8 +0,0 @@
-const ulElement = document.querySelectorAll('.item');
-console.log(`В списке ${ulElement.length} категории.`);
-
-const titles = [];
-ulElement.forEach(elem => {
-  console.log('Категория: ', elem.getElementsByTagName('h2')[0].textContent);
-  console.log('Количество эелментов: ', elem.getElementsByTagName('li').length);
-});
